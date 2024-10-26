a535ac07a9e3ef498fd8bf877233c9f924f03fcc
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-26 22:48:08 +0300

    add SecurityConstants class

---
+++ Backend/src/main/server/config/security/SecurityConstants.java
@@ -0,0 +1,10 @@
+package main.server.config.security;
+
+public class SecurityConstants {
+	public static final String AUTH_ACCESS_KEY = "access-token";
+	public static final String AUTH_REFRESH_KEY = "refresh-token";
+	public static final int AUTH_COOKIE_ACCESS_MAX_AGE = 60 * 5;
+	public static final int AUTH_COOKIE_REFRESH_MAX_AGE = 60 * 60 * 24;
+	public static final String[] CORS_ALLOWED_ORIGINS = {"*", "http://localhost:3000"};
+
+}
