27726afb60957d123cebd22df4116e32f28e3136
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-26 22:45:17 +0300

    create login and register record class for authentication

---
+++ Backend/src/main/server/sql/dto/auth/LoginUserRecord.java
@@ -0,0 +1,5 @@
+package main.server.sql.dto.auth;
+
+public record LoginUserRecord(String email, String password) {
+
+}
---
+++ Backend/src/main/server/sql/dto/auth/RegisterUserDto.java
@@ -0,0 +1,7 @@
+package main.server.sql.dto.auth;
+
+public record RegisterUserDto(String email, String password, String fullName) {
+
+	// getters and setters here...
+}
+
