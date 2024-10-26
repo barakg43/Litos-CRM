ac23c0219b9e9205e085f569e637122adeca875c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-26 22:47:51 +0300

    add UserController for users details data

---
+++ Backend/src/main/server/http/controlles/UserController.java
@@ -0,0 +1,38 @@
+package main.server.http.controlles;
+
+import main.server.sql.entities.UserEntity;
+import main.server.sql.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<UserEntity> authenticatedUser() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		UserEntity currentUser = (UserEntity) authentication.getPrincipal();
+
+		return ResponseEntity.ok(currentUser);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<List<UserEntity>> allUsers() {
+		List<UserEntity> users = userService.allUsers();
+
+		return ResponseEntity.ok(users);
+	}
+}
