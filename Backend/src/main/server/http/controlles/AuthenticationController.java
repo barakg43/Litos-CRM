9340b5c610c7aae630229e17ef5ae1a519e1e62c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-26 23:29:24 +0300

    add AuthenticationController for auth endpoint

---
+++ Backend/src/main/server/http/controlles/AuthenticationController.java
@@ -0,0 +1,45 @@
+package main.server.http.controlles;
+
+import main.server.config.security.JwtService;
+import main.server.sql.dto.auth.LoginResponse;
+import main.server.sql.dto.auth.LoginUserRecord;
+import main.server.sql.dto.auth.RegisterUserDto;
+import main.server.sql.entities.UserEntity;
+import main.server.sql.services.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+	private final JwtService jwtService;
+
+	private final AuthenticationService authenticationService;
+
+	public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+		this.jwtService = jwtService;
+		this.authenticationService = authenticationService;
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<UserEntity> register(@RequestBody RegisterUserDto registerUserDto) {
+		UserEntity registeredUser = authenticationService.signup(registerUserDto);
+
+		return ResponseEntity.ok(registeredUser);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserRecord loginUserDto) {
+		UserEntity authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+		String jwtToken = jwtService.generateToken(authenticatedUser);
+
+		LoginResponse loginResponse =
+				new LoginResponse(jwtToken, jwtService.getExpirationTime());
+
+		return ResponseEntity.ok(loginResponse);
+	}
+}
