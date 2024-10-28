91f9105c6b8fc5466d25dd02391b003c8b9711ee
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-28 23:39:26 +0200

    fix filterChain in SecurityConfig,fix userDetailsService  function bean

--- Backend/src/main/server/config/security/SecurityConfig.java
+++ Backend/src/main/server/config/security/SecurityConfig.java
@@ -6,16 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,37 +23,45 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 	private final UserRepository userRepository;
-	private final AuthenticationProvider authenticationProvider;
-	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	public SecurityConfig(UserRepository userRepository, AuthenticationProvider authenticationProvider,
-						  JwtAuthenticationFilter jwtAuthenticationFilter) {
+	public SecurityConfig(UserRepository userRepository) {
 		this.userRepository = userRepository;
-		this.authenticationProvider = authenticationProvider;
-		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+//		this.authenticationManager = authenticationManager;
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public JwtAuthenticationFilter authenticationJwtTokenFilter() {
+		return new JwtAuthenticationFilter();
+	}
+
+	@Bean
+	public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf(AbstractHttpConfigurer::disable)
-				.cors(Customizer.withDefaults())
+//				.cors(Customizer.withDefaults())
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/auth/**").permitAll()
+						.requestMatchers("/api/auth/**").permitAll()
+						.requestMatchers("/quik/api-docs/**").permitAll()
+						.requestMatchers("/quik/swagger-ui/**").permitAll()
+						.requestMatchers("/swagger-ui/**").permitAll()
+						.requestMatchers("/quik/api/auth/**").permitAll()
 						.anyRequest().authenticated()
 				)
-				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 //				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-				.httpBasic(Customizer.withDefaults());
+//				.httpBasic(Customizer.withDefaults());
+		http.authenticationProvider(authenticationProvider());
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
 	@Bean
 	UserDetailsService userDetailsService() {
-		return username -> userRepository.findByEmail(username)
+		return username -> userRepository.findByUsername(username)
 				.orElseThrow(() -> new UsernameNotFoundException("User not found"));
 	}
 
@@ -88,7 +96,7 @@ public class SecurityConfig {
 
 		configuration.setAllowedOrigins(List.of(SecurityConstants.CORS_ALLOWED_ORIGINS));
 		configuration.setAllowedMethods(List.of("GET", "POST"));
-//		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
