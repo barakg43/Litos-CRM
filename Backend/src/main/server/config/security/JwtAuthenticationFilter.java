4fbce75a1c4a0f87cad2cbf687c4827fbe5dcf57
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-30 14:04:31 +0200

    change auth subject from email to username

--- Backend/src/main/server/config/security/JwtAuthenticationFilter.java
+++ Backend/src/main/server/config/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
@@ -47,12 +49,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 		try {
 			final String jwt = authHeader.substring(7);
-			final String userEmail = jwtService.getUserNameFromJwtToken(jwt);
+			final String username = jwtService.getUsernameFromJwtToken(jwt);
 
 			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-			if (userEmail != null && authentication == null) {
-				UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+			if (username != null && authentication == null) {
+				UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
 				if (jwtService.validateJwtToken(jwt)) {
 					UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
