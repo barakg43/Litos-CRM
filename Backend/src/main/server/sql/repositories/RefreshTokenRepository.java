f4d42369fe7f636086fddd7739afd7064f206c6f
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-03 22:46:31 +0200

    create RefreshTokenRepository

---
+++ Backend/src/main/server/sql/repositories/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package main.server.sql.repositories;
+
+
+import main.server.sql.entities.RefreshTokenEntity;
+import main.server.sql.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
+	Optional<RefreshTokenEntity> findByToken(String token);
+
+	@Modifying
+	int deleteByUser(UserEntity user);
+}
