980c1dae0879bf8edddc8419a43e410d27b1a44f
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-14 08:59:51 +0200

    change status type in ErrorDTO from HttpStatus to int

--- Backend/src/main/server/sql/dto/ErrorDTO.java
+++ Backend/src/main/server/sql/dto/ErrorDTO.java
@@ -7,20 +7,20 @@ import org.springframework.http.HttpStatus;
 public class ErrorDTO {
 	private final String path;
 	private final String title;
-	private final HttpStatus status;
+	private final int status;
 	private final String message;
 
 	public ErrorDTO(String path, String title, HttpStatus status, Exception exception) {
 		this.path = path;
 		this.title = title;
-		this.status = status;
+		this.status = status.value();
 		this.message = exception.getMessage();
 	}
 
 	public ErrorDTO(String path, String title, HttpStatus status, String message) {
 		this.path = path;
 		this.title = title;
-		this.status = status;
+		this.status = status.value();
 		this.message = message;
 	}
 }
