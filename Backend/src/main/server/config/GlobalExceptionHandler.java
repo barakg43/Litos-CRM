6cd7afd174584a916b2a6dd40e06cdda49ce974d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-14 09:16:07 +0200

    add few exception handler to GlobalExceptionHandler

--- Backend/src/main/server/config/GlobalExceptionHandler.java
+++ Backend/src/main/server/config/GlobalExceptionHandler.java
@@ -1,10 +1,19 @@
 package main.server.config;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import main.server.exceptions.ResourceNotFoundException;
+import main.server.sql.dto.ErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,15 +21,44 @@ public class GlobalExceptionHandler {
 	// Handle a specific exception
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResourceNotFoundException(
-			ResourceNotFoundException ex, WebRequest request) {
+			ResourceNotFoundException ex, HttpServletRequest request) {
 		String message = ex.getMessage();
 		return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
 	}
 
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<?> handleEntityNotFoundException(
+			EntityNotFoundException exception, HttpServletRequest request) {
+		final Map<String, Object> body = new HashMap<>();
+		body.put("status", HttpServletResponse.SC_NOT_FOUND);
+		body.put("error", "Object not found");
+		body.put("message", exception.getMessage());
+		body.put("path", request.getRequestURI());
+		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
+
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<?> handleResourceNotFoundException(
+			NoResourceFoundException ex, HttpServletRequest request) {
+		final Map<String, Object> body = new HashMap<>();
+		body.put("status", HttpStatus.NOT_FOUND);
+		body.put("error", "Resource api not found");
+		body.put("message", ex.getMessage());
+		body.put("path", request.getContextPath());
+		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
+
 	// Handle all other exceptions
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handleGlobalException(Exception ex, WebRequest request) {
-		String message = "An unexpected error occurred.";
-		return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+	public ResponseEntity<?> handleGlobalException(Exception ex, HttpServletRequest request) {
+
+		String message = "An unexpected error occurred: " + ex.getMessage();
+		return new ResponseEntity<>(new ErrorDTO(request.getRequestURI(),
+				"Internal Server Error",
+				HttpStatus.INTERNAL_SERVER_ERROR,
+				message)
+				, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
