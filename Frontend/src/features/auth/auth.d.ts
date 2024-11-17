1a25685343d44b01e824a1c8bad4e8bd8faedd34
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-17 13:09:54 +0200

    add auth type file

---
+++ Frontend/src/features/auth/auth.d.ts
@@ -0,0 +1,7 @@
+export type LoginCredentials = { username: string; password: string };
+export type SignUpData = {
+  username: string;
+  fullName: string;
+  password: string;
+  email: string;
+};
