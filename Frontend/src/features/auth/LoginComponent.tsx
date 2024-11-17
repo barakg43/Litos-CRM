181bcbd52a9d78c180fdf9bc72533d25f14af135
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-17 23:34:50 +0200

    alot of design changes in login page

--- Frontend/src/features/auth/LoginComponent.tsx
+++ Frontend/src/features/auth/LoginComponent.tsx
@@ -1,11 +1,9 @@
 import {
-  Avatar,
   Box,
   Button,
   Flex,
   FormControl,
   FormHelperText,
-  Heading,
   Input,
   InputGroup,
   InputLeftElement,
@@ -16,6 +14,7 @@ import {
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { TbLock, TbUser } from "react-icons/tb";
+import Logo from "../../components/Logo";
 import { useLoginMutation } from "../../services/redux/api/authApi";
 import { LoginCredentials } from "./auth";
 
@@ -44,59 +43,86 @@ function LoginComponent() {
         justifyContent='center'
         alignItems='center'
       >
-        <Avatar bg='teal.500' />
-        <Heading color='teal.400'>Welcome</Heading>
-        <Box minW={{ base: "90%", md: "468px" }}>
-          <form onSubmit={handleSubmit(handleLogin)}>
-            <Stack
-              spacing={4}
-              p='1rem'
-              backgroundColor='whiteAlpha.900'
-              boxShadow='md'
+        <Flex justifyContent='center' alignItems='center' h='15rem'>
+          <Logo />
+        </Flex>
+        {/* <Avatar bg='teal.500' /> */}
+        {/* <Heading color='teal.400'>Welcome</Heading> */}
+        <Box as='form' onSubmit={handleSubmit(handleLogin)}>
+          {/* <form> */}
+          <Stack
+            spacing={4}
+            p='1rem'
+            backgroundColor='whiteAlpha.900'
+            boxShadow='md'
+            width={"30rem"}
+            alignItems='center'
+            borderRadius={"1rem"}
+          >
+            <FormControl
+              display='flex'
+              justifyContent={"center"}
+              alignContent={"center"}
             >
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents='none'
-                    children={<TbUser color='gray.300' />}
-                  />
-                  <Input type='email' placeholder='email address' />
-                </InputGroup>
-              </FormControl>
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents='none'
-                    color='gray.300'
-                    children={<TbLock color='gray.300' />}
-                  />
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    placeholder='Password'
-                  />
-                  <InputRightElement width='4.5rem'>
-                    <Button h='1.75rem' size='sm' onClick={handleShowClick}>
-                      {showPassword ? "Hide" : "Show"}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-                <FormHelperText textAlign='right'>
-                  <Link>forgot password?</Link>
-                </FormHelperText>
-              </FormControl>
-              <Button
-                borderRadius={0}
-                type='submit'
-                variant='solid'
-                colorScheme='teal'
-                width='full'
-                isLoading={isLoading}
-                disabled={!formState.isValid || isLoading}
-              >
-                Login
-              </Button>
-            </Stack>
-          </form>
+              <InputGroup width='20rem' height='3rem'>
+                <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.700'
+                  children={<TbUser />}
+                  height='3rem'
+                />
+                <Input
+                  type='text'
+                  placeholder='Username'
+                  height='3rem'
+                  fontSize='1.2rem'
+                  autoComplete='username'
+                  name='username'
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl width={"20rem"}>
+              <InputGroup height={"3rem"}>
+                <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.700'
+                  children={<TbLock />}
+                  height={"3rem"}
+                  alignContent='center'
+                  textAlign='center'
+                />
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  placeholder='Password'
+                  fontSize='1.2rem'
+                  height='3rem'
+                  autoComplete='password'
+                  name='password'
+                />
+                <InputRightElement width='4.5rem' height='3rem'>
+                  <Button h='1.75rem' size='sm' onClick={handleShowClick}>
+                    {showPassword ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+              <FormHelperText textAlign='right' fontSize={"1.1rem"}>
+                <Link>forgot password?</Link>
+              </FormHelperText>
+            </FormControl>
+            <Button
+              borderRadius={3}
+              type='submit'
+              variant='solid'
+              fontSize='1.3rem'
+              colorScheme='teal'
+              isLoading={isLoading}
+              disabled={!formState.isValid || isLoading}
+              width={"20rem"}
+            >
+              Login
+            </Button>
+          </Stack>
+          {/* </form> */}
         </Box>
       </Stack>
       <Box>
