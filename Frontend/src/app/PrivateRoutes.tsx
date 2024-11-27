d8c0a4747150e94ccbe29ed37b586bf054f57782
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-27 17:20:33 +0200

    handle case user already auth in PrivateRoutes,try to get user data

--- Frontend/src/app/PrivateRoutes.tsx
+++ Frontend/src/app/PrivateRoutes.tsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useRetrieveUserQuery } from "../services/redux/api/apiAuth";
 import { useAuth } from "../services/redux/slices/authStore";
 
 const PrivateRoutes = () => {
-  const auth = useAuth();
-  if (!auth.isAuthenticated) return <Navigate to='/login' />;
-  return <Outlet />;
+  const isAlreadyAuthenticated = useAuth((state) => state.isAuthenticated);
+  const login = useAuth((state) => state.login);
+  const { data: user, isLoading } = useRetrieveUserQuery(undefined, {
+    refetchInterval: false,
+    retry: false,
+  });
+  useEffect(() => {
+    if (user) login(user);
+  }, [user]);
+  if (isLoading) return <LoadingSpinner />;
+  return isAlreadyAuthenticated ? <Outlet /> : <Navigate to='/login' replace />;
 };
 
 export default PrivateRoutes;
