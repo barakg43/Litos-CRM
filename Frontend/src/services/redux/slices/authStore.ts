84d432147f823d0dc4b9a51f7102d30ea99a7915
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-27 17:11:53 +0200

    remove all cache data of react query when logout

--- Frontend/src/services/redux/slices/authStore.ts
+++ Frontend/src/services/redux/slices/authStore.ts
@@ -1,4 +1,5 @@
 import { create, StoreApi, UseBoundStore } from "zustand";
+import { queryClient } from "../../../app/AppProviders";
 import { UserDetails } from "../../../features/auth/auth";
 type AuthState = {
   user: UserDetails | null;
@@ -14,5 +15,8 @@ export const useAuth: UseBoundStore<StoreApi<AuthState>> = create((set) => ({
   },
   logout: () => {
     set({ user: null, isAuthenticated: false });
+    queryClient.removeQueries({
+      predicate: (query) => query.queryKey[0] !== "User",
+    });
   },
 }));
