d4fd61ffdfe936458b94277d1cbc04aaa341789e
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-11-28 23:48:37 +0200

    add OptionalIfVoid type to tsHelpers

--- Frontend/src/services/react-query-toolkit/tsHelpers.ts
+++ Frontend/src/services/react-query-toolkit/tsHelpers.ts
@@ -10,6 +10,7 @@ export type HasRequiredProps<T, True, False> = NonOptionalKeys<T> extends never
 export type NonOptionalKeys<T> = {
   [K in keyof T]-?: undefined extends T[K] ? never : K;
 }[keyof T];
+export type OptionalIfVoid<T> = T extends void ? T | undefined : T;
 
 export type UnwrapPromise<T> = T extends PromiseLike<infer V> ? V : T;
 export type OmitFromUnion<T, K extends keyof T> = T extends unknown
