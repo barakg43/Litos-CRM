c9dad94119587c09895ef5dbc00245bdb9657b0c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-12-01 23:24:32 +0200

    add navigate to "/login" to logout action on useAuth

--- Frontend/src/services/redux/slices/authStore.ts
+++ Frontend/src/services/redux/slices/authStore.ts
@@ -1,5 +1,6 @@
 import { create, StoreApi, UseBoundStore } from "zustand";
 import { queryClient } from "../../../app/AppProviders";
+import { router } from "../../../app/CustomRouter";
 import { UserDetails } from "../../../features/auth/auth";
 type AuthState = {
   user: UserDetails | null;
@@ -16,5 +17,6 @@ export const useAuth: UseBoundStore<StoreApi<AuthState>> = create((set) => ({
     queryClient.removeQueries({
       predicate: (query) => query.queryKey[0] !== "User",
     });
+    router.navigate("/login");
   },
 }));
