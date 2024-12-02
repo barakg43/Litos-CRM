f762119c21704d882549321665d5d70ad0f6fb83
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-12-02 23:10:39 +0200

    fix getAuthorities in UserEntity

--- Backend/src/main/server/sql/entities/UserEntity.java
+++ Backend/src/main/server/sql/entities/UserEntity.java
@@ -59,7 +59,7 @@ public class UserEntity implements UserDetails {
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return List.of();
+		return role.getAuthorities();
 	}
 
 	@Override
