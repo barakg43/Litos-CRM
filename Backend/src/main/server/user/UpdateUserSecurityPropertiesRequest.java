83d9e5e116c41a7f20dd96625332ff6cb2052937
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-12-03 12:00:55 +0200

    add @NotNull,@Nullable to UpdateUserSecurityPropertiesRequest

--- Backend/src/main/server/user/UpdateUserSecurityPropertiesRequest.java
+++ Backend/src/main/server/user/UpdateUserSecurityPropertiesRequest.java
@@ -1,10 +1,15 @@
 package main.server.user;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateUserSecurityPropertiesRequest {
+	@NotNull
 	private String username;
+	@Nullable
 	private Role role;
+	@Nullable
 	private Boolean enabled;
 }
