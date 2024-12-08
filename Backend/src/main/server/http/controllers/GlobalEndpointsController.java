e490e63fbe510073571da3d8a3c8cd8034b4e6fc
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-12-08 12:04:32 +0200

    fix the bug of foward not working  to spa application

--- Backend/src/main/server/http/controllers/GlobalEndpointsController.java
+++ Backend/src/main/server/http/controllers/GlobalEndpointsController.java
@@ -1,17 +1,22 @@
 package main.server.http.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static main.server.uilities.UtilityFunctions.printPWD;
 
-@RestController
+/**
+ * Don't use @RestController annotation here.
+ * this cause a bug in 'getSpaApplication' and  'forward' endpoints
+ * this adds @ResponseBody to endpoints which return String instead doing a redirect command to index.html
+ */
+@Controller
 public class GlobalEndpointsController {
 	private final String healthResponse = String.format("{\"boot-time\":\"%s\"}",
 			LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss")));
